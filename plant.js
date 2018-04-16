6a78885e950e68986acd57b8e7020fc075215c61
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2018-04-15 23:03:25 -0400

    plant logic and board implemented

--- plant.js
+++ plant.js
@@ -1,51 +1,112 @@
-//var id_count
+var logged_in_user = {"id": 0, 'firstName': "Gandalf", "lastName": "The Grey"};
 
-class plant {
+var uniqueID = (function() {
+   var id = 0;
+   return function() { return id++; };
+})();
+
+class Plant {
+
+	//{'name': null, 'type': null, 'watering_freq': [[0,0,0,0,0,0,0],null], }
+
+	constructor(photo_url, name, type, watering_freq, fertilizer_freq, pesticide_freq, health, light, trimming, ...args){
+		let keys = ['name', 'type', 'watering_freq', 'fertilizer_freq', 'pesticide_freq', 'health', 'light', 'trimming'];
+		for (let index = 0 ; index < keys.length ; index++)
+				this[keys[index]] = arguments[index + 1];
+
+    this.status = new Status();
+    this.photo_url = photo_url;
+
+    if (!args.length){
+      this.id = uniqueID();
+      this.owner = logged_in_user.id;
+    }
+    else { //for registering plants into the system, since we don't have a backend
+      this.id = args[0];
+      this.owner = args[1];
+    }
 
 
-	constructor(name, type, watering_freq, ...){
-		//sets properties
-		//picture file
-		//have an incremental id
-		//makes an instance of status 
 	}
 
-	destructor() {
-		//delete the obj
+	updateProperties(prop_obj){
+		for (let key of prop_obj)
+			this[key] = prop_obj[key];
 	}
 
-	getProperties(){
-		return {}
+	requestForCare(start_date, end_date){
+		this.status.update(1, start_date, end_date)
 	}
 
-	updateProperties({propertynames: value}){
-
-		for (let key of obj)
-			//change only what is in the list
-
+	cancelRequestForcare(){
+		this.status.update(0);
 	}
 
-	updateStatus(int status, additional_args){
-		//calls update status
+	requestToCare(){
+		this.status.addRequester(logged_in_user.id);
 	}
 
+	cancelRequestToCare(){
+		this.status.removeRequester(logged_in_user.id);
+	}
 
+	approveCareRequest(requester_id){
+		this.status.update(2);
+	}
+
+	cancelCareReqApproval(){
+		this.status.revertApproval();
+	}
+
+	transitionDone(){
+		this.status.update(3);
+	}
 
 
 }
 
-class status {
-	//start_date
-	//end_date
+//status_codes: 0-> at_home, 1 -> care_requested 2-> in_transition 3-> in_care
 
-	//TODO: place caretakers somewhere (an array)
-	// a variable for final caretaker
-
+class Status{
 	constructor(){
-		//at home
+		this.update(0);
 	}
 
-	update(status_code){
 
+	update(status_code, ...args){
+		if (status_code == 0){
+			this.start_date = null;
+			this.end_date = null;
+			this.req_caretakers = [];
+			this.app_caretaker = null;
+		}
+		else if (status_code == 1){
+		 	this.start_date = args[0];
+			this.end_date = args[1];
+		 }
+		else if (status_code == 2){
+			this.app_caretaker = args[0];
+		}
+
+		this.status_code = status_code;
+ 	}
+
+	addRequester(){
+		this.req_caretakers.push(logged_in_user.id);
 	}
-}
+
+	removeRequester(){
+		this.req_caretakers.splice(this.req_caretakers.indexOf(logged_in_user.id), 1);
+	}
+
+	revertApproval(){
+		this.app_caretaker = null;
+		this.status_code = 1;
+	}
+
+ }
+
+Sauron = new Plant(null,"Sauron", "Rose", [[1,0,0,1,0,0,0], 2], [[1,0,0,1,0,1,0], 1], [[1,0,1,1,0,0,0], 2], 1, 0, 1);
+Sauron.requestForCare(2, 3);
+
+Balrog = new Plant(null,"Balrog", "Orchid", [[1,0,0,1,0,0,0], 1], [[1,0,0,1,0,1,0], 0], [[1,0,1,1,0,0,0], 2], 0, 1, 1);
--- plantComponent.js
+++ plantComponent.js
@@ -1,11 +1,161 @@
 //view of the plant logic
+DEFAULT_PHOTO_URL = "./images/plant_photoholder.png"
 
+function createStatus(plant_instance, footer){
+  let text = document.createElement("p");
+  let status_code = plant_instance.status.status_code;
 
-function createPlantTile(plant_instance, container_id){
-	//make a board
-	//getplantProperties
-	//create the tile based on plant properties	
-	//appends it to the designated place for tiles
+  let extra;
+
+  if ( status_code == 0){
+    text.innerText = "Currently at home";
+
+    extra = document.createElement("div");
+    extra.classList.add("input-group-text", "aggregation-checkbox");
+
+    let checkbox = Util.create("input", {"type": "checkbox", "aria-label": "Checkbox for selecting this plant card"});
+    extra.appendChild(checkbox);
+  }
+  else if (status_code == 1){
+    text.innerText = "Requested plantsitting from " + plant_instance.status.start_date +
+    " to " + plant_instance.status.end_date;
+
+    extra = Helpers.createCancelButton();
+  }
+  else if (status_code == 2){
+    text.innerText = "Claimed for plantsitting by " + plant_instance.status.app_caretaker +
+    ". Not yet in their care.";
+
+    extra = Util.create("div");
+    extra.appendChild(Helpers.createCheckButton("Picked up?"))
+    extra.appendChild(Helpers.createCancelButton());
+
+  }
+
+  else{ //if status_code == 3
+    text.innerText = "Sent for plantsitting from " + plant_instance.status.start_date +
+    " to " + plant_instance.status.end_date;
+
+    extra = Util.create("button")
+    extra.classList.add("btn", "btn-dark", "req-photo");
+    extra.innerText = "Request photo";
+  }
+
+  footer.appendChild(text);
+  footer.appendChild(extra);
+
+  return footer;
 }
 
 
+function createIcons(plant_instance){
+  let container = Util.create("div");
+  let health, light, trimming;
+
+  if (plant_instance.health)
+    health = Helpers.createIcon("heartbeat",'icon-group', 'green');
+  else
+    health = Helpers.createIcon("heartbeat",'icon-group', 'grey', 'transparent');
+
+  container.appendChild(health);
+
+  water_container = Util.create("span");
+  for (let i = 0 ; i < plant_instance.watering_freq[1] + 1; i++){
+      water_container.appendChild(Helpers.createIcon("tint", "blue"));
+  }
+  water_container.classList.add("icon-group");
+  container.appendChild(water_container);
+
+  if (plant_instance.light)
+    light = Helpers.createIcon("sun", 'icon-group', "gold");
+  else
+    light = Helpers.createIcon("sun", 'icon-group', 'transparent');
+
+  container.appendChild(light);
+
+  fertilizer_container = Util.create("span");
+  for (let i = 0 ; i < plant_instance.fertilizer_freq[1]+ 1; i++){
+      fertilizer_container.appendChild(Helpers.createIcon("poo", 'brown'));
+  }
+  fertilizer_container.classList.add("icon-group");
+  container.appendChild(fertilizer_container);
+
+  if (plant_instance.pesticide_freq[1])
+    container.appendChild(Helpers.createIcon("bug", 'icon-group'));
+
+  if (plant_instance.trimming)
+    container.appendChild(Helpers.createIcon("cut", "icon-group"))
+
+  return container;
+}
+
+
+function createPlantTile(plant_instance, container_id){
+
+  let card = document.createElement("div");
+  card.classList.add("card");
+
+  let header = document.createElement("div");
+  header.classList.add("card-header");
+  header.innerText = "Owned by ";
+
+  let profile_link = Util.create("a", {"href": "#"});
+  profile_link.innerText = logged_in_user.firstName + " " +
+  logged_in_user.lastName;
+
+  header.appendChild(profile_link);
+
+  card.appendChild(header);
+
+  let top_img = document.createElement("img");
+  top_img.classList.add("card-img-top");
+
+  if (plant_instance.photo_url)
+    top_img.setAttribute("src", plant_instance.photo_url);
+  else{
+    top_img.setAttribute("src", DEFAULT_PHOTO_URL)
+    //top_img.classList.add("transparent");
+  }
+
+  card.appendChild(top_img);
+
+  let card_body = document.createElement("div");
+  card_body.classList.add("card-body");
+
+  let title = document.createElement("h5");
+  title.classList.add("card-title");
+  title.innerText = plant_instance.name? plant_instance.name : "";
+
+  let text = document.createElement("p");
+  text.classList.add("card-text");
+  text.innerText = plant_instance.type;
+
+  let icons = createIcons(plant_instance);
+
+  card_body.appendChild(title);
+  card_body.appendChild(text);
+  card_body.appendChild(icons);
+
+  card.appendChild(card_body);
+
+  let footer = document.createElement("div");
+  footer.classList.add("card-footer", "text-muted");
+
+  let status_footer = createStatus(plant_instance, footer);
+
+  card.appendChild(status_footer);
+
+  Util.one("#board_container").appendChild(card);
+
+	//create the tile based on plant properties
+}
+
+Util.events(document, {
+	// Final initalization entry point: the Javascript code inside this block
+	// runs at the end of start-up when the DOM is ready
+
+	"DOMContentLoaded": function() {
+
+      createPlantTile(Sauron);
+      createPlantTile(Balrog)
+    }})
