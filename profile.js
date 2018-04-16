3a1afcc31a5dc3798506fec206c5281f2f4bd387
geetickachauhan <gchau005@fiu.edu>
2018-04-15 20:51:33 -0400

    One more fix to shared.css

--- profile.js
+++ profile.js
@@ -1,2 +1,6 @@
 //listeners for profile.js buttons and inputs
 //dynamically loads the content of profile
+function showProfile(own){
+    // own is a flag that if set to 1, displays calendar, edit and add event to calendar
+    
+}
