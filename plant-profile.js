234993f6d53f87a6db41168d933fab46d3381383
geetickachauhan <gchau005@fiu.edu>
2018-04-24 22:57:23 -0400

    Partially done with plant profile editing

--- plant-profile.js
+++ plant-profile.js
@@ -48,16 +48,66 @@ function showProfile(own, photo, name, type, watering, fertilizer, pesticide, he
                 </button>`; 
             Util.one('#g-plantname').innerHTML = '<input type="text" class="form-control" id="plantname-input">';
             Util.one('#g-planttype').innerHTML = '<input type="text" class="form-control" id="planttype-input">';
-            // TODO
-            // now just use the add plant component's methods to create the radio buttons and the
-            // weekday selectors
-            // only issue is have to make sure the correct weekday selector is already selected
+           
+            Util.removeAllChildren(Util.one('#g-watering'));
+            addWeekdaySelector(Util.one('#g-watering'), 1);
+
             
+            Util.removeAllChildren(Util.one('#g-fertilizer'));
+            addWeekdaySelector(Util.one('#g-fertilizer'), 2);
+            
+            Util.removeAllChildren(Util.one('#g-pesticide'));
+            addWeekdaySelector(Util.one('#g-pesticide'), 3);
+            
+            var values = ["everyweek", "every2weeks", "everymonth"];
+            var labels = ["Every Week", "Every 2 Weeks", "Every Month"];
+            Util.removeAllChildren(Util.one('#g-watering-freq'));
+            addRadioButtons(Util.one('#g-watering-freq'), "watering", values, labels);
+            
+            Util.removeAllChildren(Util.one('#g-fertilizer-freq'));
+            addRadioButtons(Util.one('#g-fertilizer-freq'), "fertilizer", values, labels);
+            
+            Util.removeAllChildren(Util.one('#g-pesticide-freq'));
+            addRadioButtons(Util.one('#g-pesticide-freq'), "pesticide", values, labels);
+            
+            Util.removeAllChildren(Util.one('#g-healthstatus'));
+            addRadioButtons(Util.one('#g-healthstatus'), "healthstatus", ['healthy', 'sick'], ['Healthy', 'Sick']);
+            
+            Util.removeAllChildren(Util.one('#g-light'));
+            addRadioButtons(Util.one('#g-light'), "light", ['direct', 'indirect'], ['Direct', 'Indirect']);
+            
+            Util.removeAllChildren(Util.one('#g-trimming'));
+            addRadioButtons(Util.one('#g-trimming'), "trimming", ['yes', 'no'], ['Yes', 'No']);
+            
+             // TODO
+            // only issue is have to make sure the correct weekday selector is already selected
+            //             input.setAttribute("id", "weekday-"+weekdays[i] + id);
+//            var weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+//    var labels = ["M", "T", "W", "T", "F", "S", "S"];
+            Util.one('#g-specialinstructions').innerHTML = '<textarea class="form-control" id="specialinstructions-input" rows="4"></textarea>';
+            Util.one('#specialinstructions-input').value = current_plant['instructions'];
             Util.one('#plantname-input').value = current_plant['name'];
             Util.one('#planttype-input').value = current_plant['type'];
             Util.one('#g-edit-button').classList.add('g-display-none');
             Util.one('#g-save-button').classList.remove('g-display-none');
         });
+           
+//             Util.one('#save-button').addEventListener('click', function(){
+//            // find a way to edit the json based on logged in user
+//            logged_in_user['firstName'] = Util.one('#firstName-input').value;
+//            logged_in_user['lastName'] = Util.one('#lastName-input').value;
+//            logged_in_user['phone'] = Util.one('#phone-input').value;
+//            logged_in_user['email'] = Util.one('#email-input').value;
+//            logged_in_user['description'] = Util.one('#description-input').value;
+//            // basically the user will have different stuff during the session but no database changes
+//            // have been made
+//            Util.one('#name').innerHTML = logged_in_user['firstName'] + " " + logged_in_user["lastName"];
+//            Util.one('#phone').innerHTML = logged_in_user['phone'];
+//            Util.one('#email').innerHTML = logged_in_user['email']; 
+//            Util.one('#description').innerHTML = logged_in_user['description'];
+//            Util.one('#save-button').classList.add('g-display-none');
+//            Util.one('#edit-button').classList.remove('g-display-none');
+//    });
     }
     
 }
@@ -65,6 +115,7 @@ function showProfile(own, photo, name, type, watering, fertilizer, pesticide, he
 function appendWeekdayDivs(elt, vals){
     for(i=0; i<vals.length; i++){
         div = Util.create('div');
+        div.classList.add("weekday-div");
         if(vals[i] == 1){
             div.classList.add("weekday-select");
         }
