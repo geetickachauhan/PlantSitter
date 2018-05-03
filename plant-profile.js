239551656d2312da832600bbcc2a0299ec9c85ec
geetickachauhan <gchau005@fiu.edu>
2018-05-03 16:50:10 -0400

    Validation done for plant profile

--- plant-profile.js
+++ plant-profile.js
@@ -3,6 +3,7 @@ var freq_dict = {0: 'Every Week', 1: 'Every 2 Weeks', 2: 'Every Month'};
 var light_dict = {0: 'Direct', 1: 'Indirect'};
 var health_dict = {0: 'Healthy', 1: 'Sick'};
 var trimming_dict = {0: 'Yes', 1: 'No'};
+var pnvalid = true, ptvalid = true, watervalid = true, fertilizervalid = true, pesticidevalid = true; // this is to check if the plant name and type are written in the correct format
 
 $(document).ready(function(){
     //    showProfile(own, photo, star, name, phone, email, description);
@@ -93,8 +94,78 @@ function readRadioButtons(name, values){
     }
     return val;
 }
+function addWeekdaySelector(elt, id, variable){
+    // pass in variable as a string - it'll be wateringvalid etc depending on what element is being looked at
+    console.log("In plant-profile.js");
+    var selector = document.createElement("div");
+    selector.classList.add("weekDays-selector");
+    var weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    var labels = ["M", "T", "W", "T", "F", "S", "S"];
+    for(i=0; i<weekdays.length; i++){
+        var input = Util.create("input", {'type': 'checkbox', 'id': "weekday-"+weekdays[i] + id, 'onclick': 'checkCheckBoxes(this, "' + id + '", "e-'+id + '", "'+variable+'")'});
+        input.classList.add("weekday");
+//        var input = document.createElement("input");
+//        input.setAttribute("type", "checkbox");
+//        input.setAttribute("id", "weekday-"+weekdays[i] + id);
+        input.setAttribute("class", "weekday");
+        selector.appendChild(input);
+        var label = document.createElement("label");
+        label.setAttribute("for", "weekday-"+weekdays[i] + id);
+        label.innerHTML = labels[i];
+        selector.appendChild(label);
+    }
+    elt.appendChild(selector);
+    errormessage = Util.create("div", {'id': "e-"+id});
+    errormessage.classList.add("invalid-feedback");
+    errormessage.innerHTML = "Please select at least one checkbox";
+    elt.appendChild(errormessage);
+}
 function selectRadioButtons(name, values, vals){
     Util.one("#radio-" + name + values[vals]).checked = true;
+}
+// below will be useful
+function checkPattern(input, pattern, errormessageid, variable){
+    console.log("checking!");
+    
+    var regex = new RegExp(pattern);
+    if(!!regex.test(input.value) == false){
+        input.style.setProperty('border-color',  'var(--red)');
+        Util.one('#'+errormessageid).style.setProperty('display', 'inline');
+        console.log("invalid");
+        window[variable] = false;
+    }
+    else{
+        input.style.setProperty('border',  '1px solid #ced4da');
+        Util.one('#'+errormessageid).style.setProperty('display', 'none');
+        window[variable] = true;
+        console.log("valid");
+    }
+}
+function checkCheckBoxes(current, id, errormessageid, variable){
+    // TODO: check for all checkboxes, if all of them have the value 0, then show the error message
+//    <input type="text" class="form-control" id="plantname-input" oninput="checkPattern(this, '^[A-z]+$', 'e-plantname', 'pnvalid')">
+//                                <div class="invalid-feedback g-small-text" id="e-plantname">Please enter only alphabets</div>
+    // if all of them are unchecked then you should set the variable as false and then show the error message
+    // this is technically only relevant if the checkbox is unchecked
+    var weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+    var nonechecked = true; // we are going to assume at first that none are checked and then it'll be proven wrong by the first checked checkbox
+    for(i=0; i <weekdays.length; i++){
+        var input = Util.one('#weekday-'+weekdays[i] + id);
+        if(input.checked == true){
+            nonechecked = false;
+            break;
+        }
+    }
+    if(nonechecked == true){
+        // set that variable to false because the variable is no longer valid
+        window[variable] = false;
+        Util.one('#' + errormessageid).style.setProperty('display', 'inline');
+    }
+    else{
+        window[variable] = true;
+        Util.one('#' + errormessageid).style.setProperty('display', 'none');
+    }
+    
 }
 function edit_enable(current_plant){
     edit_elt = Util.one('#g-edit');
@@ -112,19 +183,33 @@ function edit_enable(current_plant){
                 <span class="fas fa-camera fa-5x"></span>
                 <input type="file" hidden>
             </label>`; 
-        Util.one('#g-plantname').innerHTML = '<input type="text" class="form-control" id="plantname-input">';
-        Util.one('#g-planttype').innerHTML = '<input type="text" class="form-control" id="planttype-input">';
+        
+        
+        plantname = Util.one('#g-plantname');
+        plantname.classList.add("form-group");
+        plantname.innerHTML = `<input type="text" class="form-control" id="plantname-input" oninput="checkPattern(this, '^[A-z]+$', 'e-plantname', 'pnvalid')">
+                                <div class="invalid-feedback g-small-text" id="e-plantname">Please enter only alphabets</div>`;
+        
+//        Util.one('#g-plantname').innerHTML = '<input type="text" class="form-control" id="plantname-input">';
+        planttype = Util.one('#g-planttype');
+        planttype.classList.add("form-group");
+        planttype.innerHTML = `<input type="text" class="form-control" id="planttype-input" oninput="checkPattern(this, '^[A-z]+$', 'e-planttype', 'ptvalid')">
+                                <div class="invalid-feedback" id="e-planttype">Alphabets only!</div>`;
+//        Util.one('#g-planttype').innerHTML = '<input type="text" class="form-control" id="planttype-input">';
 
         Util.removeAllChildren(Util.one('#g-watering'));
-        addWeekdaySelector(Util.one('#g-watering'), "watering");
+        addWeekdaySelector(Util.one('#g-watering'), "watering", "watervalid");
+        // TODO insert an invalid feedback div that appears if you uncheck the checkboxes and you try to press save
+        // may need an onclick listener on the checkboxes to check if any of the checkboxes become 0, then we know that there is a problem
+        // this idea is way too extended and we could simply have the errors for the checkboxes appear seperately 
         selectWeekdayCheckBox(watering[0], "watering");
 
         Util.removeAllChildren(Util.one('#g-fertilizer'));
-        addWeekdaySelector(Util.one('#g-fertilizer'), "fertilizer");
+        addWeekdaySelector(Util.one('#g-fertilizer'), "fertilizer", "fertilizervalid");
         selectWeekdayCheckBox(fertilizer[0], "fertilizer");
 
         Util.removeAllChildren(Util.one('#g-pesticide'));
-        addWeekdaySelector(Util.one('#g-pesticide'), "pesticide");
+        addWeekdaySelector(Util.one('#g-pesticide'), "pesticide", "pesticidevalid");
         selectWeekdayCheckBox(pesticide[0], "pesticide");
 
         var values = ["everyweek", "every2weeks", "everymonth"];
@@ -164,7 +249,13 @@ function edit_enable(current_plant){
        // TODO: make the save button work
         Util.one('#g-save-button').addEventListener('click', function(){
             // find a way to edit the json based on logged in user
-            // read the buttons and then store it in the variable -- ask farnaz how she does this. Is there a function for this.
+            if(pnvalid == false || ptvalid == false || watervalid == false || fertilizervalid == false || pesticidevalid == false){
+                console.log("one of the inputs was not valid");
+                return;
+            }
+            // TODO just check over here if the watering, fertlizer and pesticide frequency are ok.
+            // basically none of the checkboxes should ever be empty when save is pressed
+            // when you read watering[0] or fertilizer[0] or pesticide[0], it should never be 0000
             current_plant['name'] = Util.one('#plantname-input').value;
             current_plant['type'] = Util.one('#planttype-input').value;
             current_plant['instructions'] = Util.one('#specialinstructions-input').value;
